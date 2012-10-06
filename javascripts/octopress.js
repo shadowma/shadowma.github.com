8b1ed533132b0d2e481fd479cb72828c1a67dc85
Shadow Ma <i@shadow.ma>
2012-10-07 03:47:04 +0800

    Site updated at 2012-10-06 19:47:04 UTC

--- javascripts/octopress.js
+++ javascripts/octopress.js
@@ -107,15 +107,6 @@ function wrapFlashVideos() {
   });
 }
 
-function renderDeliciousLinks(items) {
-  var output = "<ul>";
-  for (var i=0,l=items.length; i<l; i++) {
-    output += '<li><a href="' + items[i].u + '" title="Tags: ' + (items[i].t == "" ? "" : items[i].t.join(', ')) + '">' + items[i].d + '</a></li>';
-  }
-  output += "</ul>";
-  $('#delicious').html(output);
-}
-
 $.domReady(function() {
   testFeatures();
   wrapFlashVideos();
