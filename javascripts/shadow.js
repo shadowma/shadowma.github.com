85479865c7e3b75bfb35fa419a41054074d17606
Shadow Ma <i@shadow.ma>
2013-01-04 11:54:29 +0800

    Site updated at 2013-01-04 03:54:29 UTC

--- javascripts/shadow.js
+++ javascripts/shadow.js
@@ -125,9 +125,92 @@
 	    }
 	};
 	
+	var Footnotes = {
+	    footnotetimeout: false,
+	    iOS: function() {
+		    var agent = navigator.userAgent.toLowerCase();
+		    return (
+			    agent.indexOf('iphone') >= 0
+		    ||
+			    agent.indexOf('ipad') >= 0
+		    ||
+			    agent.indexOf('ipod') >= 0
+		    );
+	    },
+	    setup: function() {
+		    var footnotelinks = jQuery("a[rel='footnote']")
+		
+		    footnotelinks.unbind('mouseover',Footnotes.footnoteover);
+		    footnotelinks.unbind('mouseout',Footnotes.footnoteoout);
+		
+		    footnotelinks.bind('mouseover',Footnotes.footnoteover);
+		    footnotelinks.bind('mouseout',Footnotes.footnoteoout);
+	    },
+	    footnoteover: function() {
+		    clearTimeout(Footnotes.footnotetimeout);
+		    jQuery('#footnotediv').stop();
+		    jQuery('#footnotediv').remove();
+		
+		    var id = jQuery(this).attr('href').substr(1);
+		    var position = jQuery(this).offset();
+	
+		    var d = document.createElement('div');
+		    var div = jQuery(d);
+		    div.attr('id','footnotediv');
+		
+		    if(Footnotes.iOS()) {
+			    // don't bind the mouseover event here; otherwise, Safari for iOS
+			    // will only trigger the mouseover event when the user taps the
+			    // popup, and will not fire the remaining events
+			    // see here: http://developer.apple.com/library/ios/#DOCUMENTATION/AppleApplications/Reference/SafariWebContent/HandlingEvents/HandlingEvents.html
+			    div.bind('click',Footnotes.remove);
+		    } else {
+			    div.bind('mouseover',Footnotes.divover);
+			    div.bind('mouseout',Footnotes.footnoteoout);
+		    }
+
+		    var el = document.getElementById(id);
+		    div.html('<div>'+jQuery(el).html()+'</div>');
+		    div.find("a[rel='reference']").remove();
+		
+		    jQuery(document.body).append(div);
+
+		    var left = position.left;
+		    if(left + 420  > jQuery(window).width() + jQuery(window).scrollLeft())
+			    left = jQuery(window).width() - 420 + jQuery(window).scrollLeft();
+		    var top = position.top+20;
+		    if(top + div.height() > jQuery(window).height() + jQuery(window).scrollTop())
+			    top = position.top - div.height() - 15;
+		    div.css({
+			    left:left,
+			    top:top,
+		    });
+	    },
+	    footnoteoout: function() {
+		    Footnotes.footnotetimeout = setTimeout(function() {
+			    Footnotes.remove();
+		    },100);
+	    },
+	    remove: function() {
+		    jQuery('#footnotediv').animate({
+			    opacity: 0
+		    }, 600, function() {
+			    jQuery('#footnotediv').remove();
+		    });
+	    },
+	    divover: function() {
+		    clearTimeout(Footnotes.footnotetimeout);
+		    jQuery('#footnotediv').stop();
+		    jQuery('#footnotediv').css({
+				    opacity: 1
+		    });
+	    }
+	}
+	
 	scrolltotop.init();
 	externalLinks(); // Delete or comment this line to disable opening external links in new window
 	appendCaption(); // Delete or comment this line to disable caption
+	Footnotes.setup();
 })(jQuery);
 
 var up = function(){
